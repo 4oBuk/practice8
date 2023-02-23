1e8e0a467791e87172379a8e03e5885fbc7ccce4
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-02-23 17:57:33 +0200

    init commit

---
+++ src/main/java/com/chornobuk/practice8/celebapi/CelebapiApplication.java
@@ -0,0 +1,13 @@
+package com.chornobuk.practice8.celebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chornobuk/practice8/celebapi/CelebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chornobuk.practice8.celebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
